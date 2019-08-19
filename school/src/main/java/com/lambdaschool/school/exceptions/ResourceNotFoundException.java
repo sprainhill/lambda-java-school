0b3491a6719868ca6c93840be853e805eac4ff24
Rory Murray <rory.christian.murray@gmail.com>
2019-08-19 18:42:33 -0400

    Build ValidationError

--- school/src/main/java/com/lambdaschool/school/exceptions/ResourceNotFoundException.java
+++ school/src/main/java/com/lambdaschool/school/exceptions/ResourceNotFoundException.java
@@ -17,5 +17,5 @@ public class ResourceNotFoundException extends RuntimeException
     {
         super(message, cause);
     }
-    
+
 }
--- school/src/main/java/com/lambdaschool/school/exceptions/ValidationError.java
+++ school/src/main/java/com/lambdaschool/school/exceptions/ValidationError.java
@@ -1,5 +1,32 @@
 package com.lambdaschool.school.exceptions;
 
+
+
 public class ValidationError
 {
+    private String code;
+    private String message;
+
+    // generate getters and setters
+
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public void setCode(String code)
+    {
+        this.code = code;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
 }
