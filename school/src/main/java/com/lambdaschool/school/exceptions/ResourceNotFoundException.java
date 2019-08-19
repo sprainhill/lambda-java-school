d0c8fe01b10f76a4db6b7986bb39fc3ac6439eb0
Rory Murray <rory.christian.murray@gmail.com>
2019-08-19 18:40:25 -0400

    Add ResourceNotFound code and annotations

--- school/src/main/java/com/lambdaschool/school/exceptions/ResourceNotFoundException.java
+++ school/src/main/java/com/lambdaschool/school/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,21 @@
 package com.lambdaschool.school.exceptions;
 
-public class ResourceNotFoundException
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// when we an http status not founbd, we're gonna do these things
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+// Were gonna take some methods off of RuntimeException to do these things
+public class ResourceNotFoundException extends RuntimeException
 {
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+    
 }
