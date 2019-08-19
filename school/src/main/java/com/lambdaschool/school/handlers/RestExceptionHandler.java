714d002c8d1795f6a81a1e984251eef2b29e320a
Rory Murray <rory.christian.murray@gmail.com>
2019-08-19 19:24:03 -0400

    Create handlers file structure

---
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.school.handlers;
+
+// The RestExceptionHandler will handle/implement the exceptions
+// we created in the exceptions package, in this case the ResourceNotFound
+// class and the ValidationError class
+
+public class RestExceptionHandler
+{
+}
