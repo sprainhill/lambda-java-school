47d9d474b0fd44c4792fdd4447539406aaf455f9
Rory Murray <rory.christian.murray@gmail.com>
2019-08-19 19:50:35 -0400

    Modify main class for exception handling

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -2,6 +2,8 @@ package com.lambdaschool.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class SchoolApplication
@@ -9,7 +11,10 @@ public class SchoolApplication
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SchoolApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SchoolApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
