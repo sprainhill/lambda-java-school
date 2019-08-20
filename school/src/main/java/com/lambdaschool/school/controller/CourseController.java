425dd5e97b6c0efbecda9b6be33df9d38be150ff
Rory Murray <rory.christian.murray@gmail.com>
2019-08-19 20:05:24 -0400

    add access logging to CourseController

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -3,38 +3,52 @@ package com.lambdaschool.school.controller;
 import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.service.CourseService;
 import com.lambdaschool.school.view.CountStudentsInCourses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/courses")
 public class CourseController
 {
+    // create logger
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class)
+
+
     @Autowired
     private CourseService courseService;
 
     @GetMapping(value = "/courses", produces = {"application/json"})
-    public ResponseEntity<?> listAllCourses()
+    public ResponseEntity<?> listAllCourses(HttpServletRequest request)
     {
         // logger will go here logging when accessed
+        logger.trace(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
         ArrayList<Course> myCourses = courseService.findAll();
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
     @GetMapping(value = "/studcount", produces = {"application/json"})
-    public ResponseEntity<?> getCountStudentsInCourses()
+    public ResponseEntity<?> getCountStudentsInCourses(HttpServletRequest request)
     {
         // logger will go here logging when accessed
+        logger.trace(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
 
     @DeleteMapping("/courses/{courseid}")
-    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
+    public ResponseEntity<?> deleteCourseById(@PathVariable long courseid, HttpServletRequest request)
     {
+        // logger will go here logging when accessed
+        logger.trace(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
         courseService.delete(courseid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
