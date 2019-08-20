f827fa319c1a8a8fd61ed4d76323c24645e5e1dd
Rory Murray <rory.christian.murray@gmail.com>
2019-08-20 17:46:46 -0400

    Add UserService

--- school/src/main/java/com/lambdaschool/school/service/UserService.java
+++ school/src/main/java/com/lambdaschool/school/service/UserService.java
@@ -1,5 +1,20 @@
 package com.lambdaschool.school.service;
 
-public interface UserService
-{
+import com.lambdaschool.school.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findUserByName(String name);
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
 }
