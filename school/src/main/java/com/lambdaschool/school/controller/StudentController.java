22886e8432d8e513ddaafeb5c444bb0d1e94fd51
Rory Murray <rory.christian.murray@gmail.com>
2019-08-20 11:58:14 -0400

    Add custom swagger documentation for StudentController endpoints

--- school/src/main/java/com/lambdaschool/school/controller/StudentController.java
+++ school/src/main/java/com/lambdaschool/school/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +28,9 @@ public class StudentController
     @Autowired
     private StudentService studentService;
 
-    // Please note there is no way to add students to course yet!
-
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // get all students, and the response will be a container of type List
+    @ApiOperation(value = "Get all students", responseContainer = "List" )
     @GetMapping(value = "/students", produces = {"application/json"})
     public ResponseEntity<?> listAllStudents(HttpServletRequest request)
     {
@@ -37,6 +40,9 @@ public class StudentController
         return new ResponseEntity<>(myStudents, HttpStatus.OK);
     }
 
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // get a student by studid, and the response will be a Student type object
+    @ApiOperation(value = "Get a student by id", response = Student.class)
     @GetMapping(value = "/Student/{StudentId}",
                 produces = {"application/json"})
     public ResponseEntity<?> getStudentById(
@@ -49,7 +55,10 @@ public class StudentController
         return new ResponseEntity<>(r, HttpStatus.OK);
     }
 
-
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // get a student with a name starting with the provided name and return a response
+    // of a list of Student types
+    @ApiOperation(value = "Get a list of students with names like provided name", responseContainer = "List")
     @GetMapping(value = "/student/namelike/{name}",
                 produces = {"application/json"})
     public ResponseEntity<?> getStudentByNameContaining(
@@ -61,7 +70,9 @@ public class StudentController
         return new ResponseEntity<>(myStudents, HttpStatus.OK);
     }
 
-
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // add a new student
+    @ApiOperation(value = "Creates a new student", notes = "newly created studentid will be provided in response header", response = void.class)
     @PostMapping(value = "/Student",
                  consumes = {"application/json"},
                  produces = {"application/json"})
@@ -80,8 +91,9 @@ public class StudentController
 
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
-
-
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // update student based on student id, or create a new student if none previously
+    @ApiOperation(value = "Updates a student based on provided id", notes = "if no student existing will create student", response = void.class)
     @PutMapping(value = "/Student/{Studentid}")
     public ResponseEntity<?> updateStudent(
             @RequestBody
@@ -95,7 +107,9 @@ public class StudentController
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
+    // Swagger annotation, giving this endpoint documentation saying that it will
+    // delete a student based on student id
+    @ApiOperation(value = "Deletes student based off id", response = void.class)
     @DeleteMapping("/Student/{Studentid}")
     public ResponseEntity<?> deleteStudentById(
             @PathVariable
