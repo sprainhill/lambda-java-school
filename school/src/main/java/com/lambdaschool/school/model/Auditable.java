42f167aa8dd02e27220f812ff33e0c4a7b2dcfa2
Rory Murray <rory.christian.murray@gmail.com>
2019-08-20 17:40:00 -0400

    Add Role model

--- school/src/main/java/com/lambdaschool/school/model/Auditable.java
+++ school/src/main/java/com/lambdaschool/school/model/Auditable.java
@@ -2,6 +2,9 @@ package com.lambdaschool.school.model;
 
 
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
