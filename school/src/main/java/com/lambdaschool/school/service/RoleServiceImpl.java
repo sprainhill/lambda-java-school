bdd573df12155fe7eceee2bb9a12b2f0090e48c0
Rory Murray <rory.christian.murray@gmail.com>
2019-08-20 17:17:47 -0400

    Create RoleServiceImpl

--- school/src/main/java/com/lambdaschool/school/service/RoleServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/RoleServiceImpl.java
@@ -1,5 +1,47 @@
 package com.lambdaschool.school.service;
 
-public class RoleServiceImpl
+import com.lambdaschool.school.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service(value = "roleService")
+public class RoleServiceImpl implements RoleService
 {
+    @Autowired
+    RoleRepository rolerepos;
+
+    @Override
+    public List<Role> findAll()
+    {
+        List<Role> list = new ArrayList<>();
+        rolerepos.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+
+    @Override
+    public Role findRoleById(long id)
+    {
+        return rolerepos.findById(id).orElseThrow(() -> new ResourceNotFoundException(Long.toString(id)));
+    }
+
+
+    @Override
+    public void delete(long id)
+    {
+        rolerepos.findById(id).orElseThrow(() -> new ResourceNotFoundException(Long.toString(id)));
+        rolerepos.deleteById(id);
+    }
+
+
+    @Transactional
+    @Override
+    public Role save(Role role)
+    {
+        return rolerepos.save(role);
+    }
 }
+
