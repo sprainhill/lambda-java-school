f57698b1915c2761c40323554b207c6dd2f210aa
Rory Murray <rory.christian.murray@gmail.com>
2019-08-20 17:17:14 -0400

    Create RoleService

--- school/src/main/java/com/lambdaschool/school/service/RoleService.java
+++ school/src/main/java/com/lambdaschool/school/service/RoleService.java
@@ -2,4 +2,11 @@ package com.lambdaschool.school.service;
 
 public interface RoleService
 {
+    List<Role> findAll();
+
+    Role findRoleById(long id);
+
+    void delete(long id);
+
+    Role save(Role role);
 }
