030b096769b9d71d6442dba95f0258965db547e2
Rory Murray <rory.christian.murray@gmail.com>
2019-08-22 17:10:56 -0400

    Add notations to main class

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -3,8 +3,14 @@ package com.lambdaschool.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableWebMvc
+@EnableJpaAuditing
+@EnableSwagger2
 @SpringBootApplication
 public class SchoolApplication
 {
